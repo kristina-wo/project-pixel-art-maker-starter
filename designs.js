a69b65f7e83410008f90118e46dda74c17627a1e
kristina-wo <kristina.wockenfuss@gmx.net>
2020-10-04 20:56:19 +0200

    create grid

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
 // Select color input
+const sizePicker = document.getElementById('sizePicker')
+const colorPicker = document.getElementById('colorPicker')
+const selColor = colorPicker.value;
+
 // Select size input
+
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(event) {
+  event.preventDefault();
 
-// Your code goes here!
+  let canvas = document.getElementById("pixelCanvas");
+  canvas.innerHTML = "";
+  let selHeight = document.getElementById('inputHeight').value;
+  let selWidth = document.getElementById('inputWidth').value;
 
+    for (let i = 0; i < selHeight; i++) {
+      let tr = canvas.insertRow();
+      for (let j = 0; j < selWidth; j++) {
+        tr.insertCell();
+      }
+    }
 }
+// Your code goes here!
+//let canvas = document.getElementById('pixelCanvas');
+//tr = Height
+//td = Width
+
+sizePicker.addEventListener("submit", makeGrid);
