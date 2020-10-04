94b74003710f93f5f3958edfa5e7551d1a914314
kristina-wo <kristina.wockenfuss@gmx.net>
2020-10-04 22:20:32 +0200

    add event listener to set background color

--- designs.js
+++ designs.js
@@ -1,31 +1,39 @@
 // Select color input
-const sizePicker = document.getElementById('sizePicker')
+// store in variable
 const colorPicker = document.getElementById('colorPicker')
-const selColor = colorPicker.value;
 
 // Select size input
-
+// store in variable
+const sizePicker = document.getElementById('sizePicker')
 
 // When size is submitted by the user, call makeGrid()
+// Your code goes here!
 
-function makeGrid(event) {
-  event.preventDefault();
-
-  let canvas = document.getElementById("pixelCanvas");
-  canvas.innerHTML = "";
+function makeGrid(myEvent) {
+  //prevent page from reloading after 'submit'
+  myEvent.preventDefault();
+  //select element with id 'pixelCanvas' and store in variable
+  let canvas = document.getElementById('pixelCanvas');
+  canvas.innerHTML = '';
+  //get value of selected height and store in variable
   let selHeight = document.getElementById('inputHeight').value;
+  //get value of selected width and store in variable
   let selWidth = document.getElementById('inputWidth').value;
-
-    for (let i = 0; i < selHeight; i++) {
+  //get value of selected color and store in variable
+  let selColor = colorPicker.value;
+    //create table rows; number of rows to create = selHeight
+    for (let i = 1; i <= selHeight; i++) {
       let tr = canvas.insertRow();
-      for (let j = 0; j < selWidth; j++) {
-        tr.insertCell();
+      //create cells in each row; number of cells to create per row = selWidth
+      for (let j = 1; j <= selWidth; j++) {
+        let td = tr.insertCell();
+        //set background color of cells to 'white'
+        td.style.backgroundColor = 'white';
+        //add event listener to each cell to set background color to selColor
+        td.addEventListener('click', function(myEvent){
+          myEvent.target.style.backgroundColor = selColor;
+        })
       }
     }
 }
-// Your code goes here!
-//let canvas = document.getElementById('pixelCanvas');
-//tr = Height
-//td = Width
-
 sizePicker.addEventListener("submit", makeGrid);
